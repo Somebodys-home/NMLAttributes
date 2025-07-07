a8cf7360a18883d2974767afe673d662053cb805
NoOne_NoBody <hypixelnerd347@gmail.com>
2025-07-06 23:03:51 -0400

    alr lets put some attributes in this bitch

--- src/main/java/io/github/Gabriel/NMLAttributes/NMLAttributes.java
+++ src/main/java/io/github/Gabriel/NMLAttributes/NMLAttributes.java
@@ -1,89 +1,21 @@
 package io.github.Gabriel.NMLAttributes;
 
 import io.github.Gabriel.NMLAttributes.attributeSystem.AttributesCommand;
-import io.github.Gabriel.NMLAttributes.energySystem.EnergyListener;
-import io.github.Gabriel.NMLAttributes.energySystem.EnergyManager;
-import io.github.Gabriel.NMLAttributes.levelSystem.*;
 import io.github.Gabriel.NMLAttributes.attributeSystem.AttributesListener;
-import io.github.Gabriel.NMLAttributes.overhealthSystem.OverhealthListener;
-import io.github.Gabriel.NMLAttributes.overhealthSystem.OverhealthManager;
-import io.github.Gabriel.NMLAttributes.profileSystem.ProfileConfig;
-import io.github.Gabriel.NMLAttributes.profileSystem.ProfileListener;
-import io.github.Gabriel.NMLAttributes.profileSystem.ProfileManager;
-import io.github.Gabriel.NMLAttributes.profileSystem.ResetProfileCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NMLAttributes extends JavaPlugin {
     private NMLAttributes instance;
-    private ProfileManager profileManager;
-    private ProfileConfig profileConfig;
-    private EnergyManager energyManager;
-    private PlayerActionBar playerActionBar;
-    private LevelManager levelManager;
-    private OverhealthManager overhealthManager;
 
     @Override
     public void onEnable() {
         instance = this;
 
-        profileConfig = new ProfileConfig(this, "profiles");
-        profileConfig.loadConfig();
-
-        profileManager = new ProfileManager(this);
-        profileManager.loadProfilesFromConfig();
-
-        energyManager = new EnergyManager(this);
-        energyManager.energyRegenServerTask();
-
-        playerActionBar = new PlayerActionBar(this);
-        playerActionBar.actionBarsTask();
-
-        levelManager = new LevelManager(this);
-        levelManager.updateLevelBarTask();
-
-        overhealthManager = new OverhealthManager(this);
-        overhealthManager.startOverhealthTracker();
-
         getCommand("attributes").setExecutor(new AttributesCommand(this));
-        getCommand("levelUp").setExecutor(new LevelUpCommand(this));
-        getCommand("setLevel").setExecutor(new SetLevelCommand());
-        getCommand("resetProfile").setExecutor(new ResetProfileCommand(this));
-
-        getServer().getPluginManager().registerEvents(new ProfileListener(this), this);
-        getServer().getPluginManager().registerEvents(new OverhealthListener(this), this);
-        getServer().getPluginManager().registerEvents(new AttributesListener(this), this);
-        getServer().getPluginManager().registerEvents(new EnergyListener(this), this);
-        getServer().getPluginManager().registerEvents(new LevelingListener(this), this);
-    }
-
-    @Override
-    public void onDisable() {
-        // DO NOT CHANGE THE ORDER OF THIS, IT WILL BREAK
-        profileManager.saveProfilesToConfig();
-        profileConfig.saveConfig();
+        getServer().getPluginManager().registerEvents(new AttributesListener(), this);
     }
 
     public NMLAttributes getInstance() {
         return instance;
-    }
-
-    public ProfileManager getProfileManager() {
-        return profileManager;
-    }
-
-    public ProfileConfig getProfileConfig() {
-        return profileConfig;
-    }
-
-    public EnergyManager getEnergyManager() {
-        return energyManager;
-    }
-
-    public LevelManager getLevelManager() {
-        return levelManager;
-    }
-
-    public OverhealthManager getOverhealthManager() {
-        return overhealthManager;
     }
 }
--- src/main/java/io/github/Gabriel/NMLAttributes/attributeSystem/AttributesListener.java
+++ src/main/java/io/github/Gabriel/NMLAttributes/attributeSystem/AttributesListener.java
@@ -1,59 +1,15 @@
 package io.github.Gabriel.NMLAttributes.attributeSystem;
 
-import io.github.Gabriel.NMLAttributes.NMLAttributes;
-import io.github.Gabriel.NMLAttributes.overhealthSystem.OverhealthChangeEvent;
-import io.github.Gabriel.NMLAttributes.profileSystem.ProfileManager;
-import org.bukkit.attribute.Attribute;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class AttributesListener implements Listener {
-    private ProfileManager profileManager;
-    private double vitalityBonus;
-    private double strengthBonus;
-    private double currentOverhealth;
-    private double maxOverhealth;
-
-    public AttributesListener(NMLAttributes nmlAttributes) {
-        profileManager = nmlAttributes.getProfileManager();
-    }
-
     @EventHandler
-    public void vitality(PlayerJoinEvent event) {
-        Player player = event.getPlayer();
-        vitalityBonus = profileManager.getPlayerProfile(player.getUniqueId()).getAttributes().getVitalityBonus();
+    public void vitality(PlayerJoinEvent event) {}
 
-        player.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(20D + vitalityBonus);
-    }
-
+    // todo: only have this damage apply to certain types of melee weapons
     @EventHandler
-    public void strength(EntityDamageByEntityEvent event) { // todo: only have this damage apply to certain types of melee weapons
-        if (event.getDamager() instanceof Player player) {
-            strengthBonus = profileManager.getPlayerProfile(player.getUniqueId()).getAttributes().getStrengthBonus();
-
-            event.setDamage(event.getDamage() * (1 + (strengthBonus / 100)));
-        }
-    }
-
-    @EventHandler
-    public void overhealth(PlayerJoinEvent event) {
-        Player player = event.getPlayer();
-        maxOverhealth = profileManager.getPlayerProfile(player.getUniqueId()).getAttributes().getMaxOverhealth();
-
-        profileManager.updateStatsFromProfile(player);
-    }
-
-    @EventHandler
-    public void updateOverhealth(OverhealthChangeEvent event) {
-        Player player = event.getPlayer();
-        Attributes attributes = profileManager.getPlayerProfile(player.getUniqueId()).getAttributes();
-
-        player.setAbsorptionAmount(event.getNewOverhealth());
-        attributes.setCurrentOverhealth(event.getNewOverhealth());
-        profileManager.getPlayerProfile(player.getUniqueId()).setAttributes(attributes);
-        profileManager.updateStatsFromProfile(player);
-    }
+    public void strength(EntityDamageByEntityEvent event) {}
 }
